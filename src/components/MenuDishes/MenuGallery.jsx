8626f407f5b104ef6345f153efacfabfdd5de2b4
joseLuisMtzE <jmartinez50@ucol.mx>
2020-04-20 01:03:28 -0500

    commit menuGallery2

--- src/components/MenuDishes/MenuGallery.jsx
+++ src/components/MenuDishes/MenuGallery.jsx
@@ -105,7 +105,29 @@ function MenuGallery() {
                 </div>
             </Col>
         </Row>
-
+        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+            <Col className="gutter-row" span={8}>
+                <div style={style}>
+                <Card hoverable style={{ width: "auto" }} cover={ <img alt="example" src="https://pbs.twimg.com/profile_images/3699449208/51ab4f23131f1ac33bc8458fae74c8bf_400x400.jpeg"/>}>
+                    <Meta style={{padding:"2px"}} title="Europe Street beat" description="www.instagram.com" />
+                </Card>
+                </div>
+            </Col>
+            <Col className="gutter-row" span={8}>
+                <div style={style}>
+                <Card hoverable style={{ width: "auto" }} cover={ <img alt="example" src="https://pbs.twimg.com/profile_images/3699449208/51ab4f23131f1ac33bc8458fae74c8bf_400x400.jpeg"/>}>
+                    <Meta style={{padding:"2px"}} title="Europe Street beat" description="www.instagram.com" />
+                </Card>
+                </div>
+            </Col>
+            <Col className="gutter-row" span={8}>
+                <div style={style}>
+                <Card hoverable style={{ width: "auto" }} cover={ <img alt="example" src="https://pbs.twimg.com/profile_images/3699449208/51ab4f23131f1ac33bc8458fae74c8bf_400x400.jpeg"/>}>
+                    <Meta style={{padding:"2px"}} title="Europe Street beat" description="www.instagram.com" />
+                </Card>
+                </div>
+            </Col>
+        </Row>
     </div>
   );
 }
