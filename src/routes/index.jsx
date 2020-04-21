15974734fe6f6057b98996d2d0babb968e129ad2
JonathanSC <jonathansolisc_22@hotmail.com>
2020-04-21 03:34:25 -0500

    Added alerts components, refactor of closeOrder, now it conects to api, makes form process and validation, removed unused css, changed layout of components

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,14 +3,13 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import 'antd/dist/antd.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 //Este es de prueba y solo ilustrativo de un spinner para mostrar mienstras los componenntes cargan
 import Spinner from '../shared/ejemplo_spinner';
 
 const Home = lazy(() => import('../pages/Home'));
-const KitchenArea = lazy(()=> import ('../pages/KitchenArea'));
-const Categories = lazy(()=> import ('../pages/Categories'));
-const closeAccount = lazy(()=> import ('../pages/closeAccount'));
+const KitchenArea = lazy(() => import('../pages/KitchenArea'));
+const Categories = lazy(() => import('../pages/Categories'));
+const CloseOrderPage = lazy(() => import('../pages/CloseOrderPage'));
 
 export default function AppRoutes() {
   return (
@@ -18,10 +17,10 @@ export default function AppRoutes() {
       <Suspense fallback={<Spinner size={130} />}>
         <BrowserRouter>
           <Switch>
-            <Route  exact path="/" component={Home} />
+            <Route exact path="/" component={Home} />
             <Route exact path="/areas-cocina" component={KitchenArea} />
             <Route exact path="/categorias-alimentos" component={Categories} />
-            <Route  exact path="/cerrar-cuenta" component={closeAccount} />
+            <Route exact path="/cerrar-orden" component={CloseOrderPage} />
           </Switch>
         </BrowserRouter>
       </Suspense>
