9dc52f50690aaa2d207b4ffb75d702b10a547359
JonathanSC <jonathansolisc_22@hotmail.com>
2020-04-22 19:39:08 -0500

    [Bug fix] missing dependencies and code due to bad conflict solving

--- src/pages/Tables.jsx
+++ src/pages/Tables.jsx
@@ -10,131 +10,130 @@ export default function Tables() {
     {
       id: 1,
       numMesa: 1,
-      estado: 'disponible',
+      estado: 'disponible'
     },
     {
       id: 2,
       numMesa: 2,
-      estado: 'ocupada',
+      estado: 'ocupada'
     },
     {
       id: 3,
       numMesa: 3,
-      estado: 'disponible',
+      estado: 'disponible'
     },
     {
       id: 4,
       numMesa: 4,
-      estado: 'reservada',
+      estado: 'reservada'
     },
     {
       id: 5,
       numMesa: 5,
-      estado: 'disponible',
+      estado: 'disponible'
     },
     {
       id: 6,
       numMesa: 6,
-      estado: 'disponible',
+      estado: 'disponible'
     },
     {
       id: 7,
       numMesa: 7,
-      estado: 'disponible',
+      estado: 'disponible'
     },
     {
       id: 8,
       numMesa: 8,
-      estado: 'ocupada',
+      estado: 'ocupada'
     },
     {
       id: 9,
       numMesa: 9,
-      estado: 'reservada',
+      estado: 'reservada'
     },
     {
       id: 10,
       numMesa: 10,
-      estado: 'disponible',
+      estado: 'disponible'
     },
     {
       id: 11,
       numMesa: 11,
-      estado: 'disponible',
+      estado: 'disponible'
     },
     {
       id: 12,
       numMesa: 12,
-      estado: 'disponible',
+      estado: 'disponible'
     },
     {
       id: 13,
       numMesa: 13,
-      estado: 'reservada',
+      estado: 'reservada'
     },
     {
       id: 14,
       numMesa: 14,
-      estado: 'disponible',
+      estado: 'disponible'
     },
     {
       id: 15,
       numMesa: 15,
-      estado: 'ocupada',
-    },
+      estado: 'ocupada'
+    }
   ]);
 
   const [state, setState] = useState({
     visible: false,
-    filter: 'todas',
+    filter: 'todas'
   });
 
   const filterOptions = [
     {
       value: 'todas',
-      label: 'Todas',
+      label: 'Todas'
     },
     {
       value: 'disponibles',
-      label: 'Disponibles',
+      label: 'Disponibles'
     },
     {
       value: 'ocupadas',
-      label: 'Ocupadas',
+      label: 'Ocupadas'
     },
     {
       value: 'reservadas',
-      label: 'Reservadas',
-    },
+      label: 'Reservadas'
+    }
   ];
 
-  const addTable = (data) => {
+  const addTable = data => {
     var temp = [...tables];
-    temp.push(data);    
+    temp.push(data);
     setTables(temp);
   };
 
-  const deleteTable = (id)=>{
+  const deleteTable = id => {
     var temp = [...tables];
-    temp.map((t,index)=>{
-      if(t.id === id)
-        temp.splice(index,1)
-    })
+    temp.forEach((t, index) => {
+      if (t.id === id) temp.splice(index, 1);
+    });
     setTables(temp);
-  }
+  };
 
   return (
     <Row>
       <Col md={24} xs={24}>
-        <img src={Background} alt="bg" style={{width:'100%',height:180}}/>
-        <header style={{ top:0,position:'absolute' }}>
+        <img src={Background} alt="bg" style={{ width: '100%', height: 180 }} />
+        <header style={{ top: 0, position: 'absolute' }}>
           <h1
             style={{
               color: '#545454',
-              position:'absolute',
-              left:20,
-              top:30,
-              fontWeight:'bold'
+              position: 'absolute',
+              left: 20,
+              top: 30,
+              fontWeight: 'bold'
             }}
           >
             Mesas
@@ -153,40 +152,56 @@ export default function Tables() {
 
         <Row>
           {state.filter === 'todas' &&
-            tables.map((table) => (
+            tables.map(table => (
               <Col md={4} xs={8} key={table.id}>
-                <Table table={table} numMesa={table.numMesa} deleteTable={deleteTable} />
+                <Table
+                  table={table}
+                  numMesa={table.numMesa}
+                  deleteTable={deleteTable}
+                />
               </Col>
             ))}
           {state.filter === 'disponibles' &&
             tables.map(
-              (table) =>
+              table =>
                 table.estado === 'disponible' && (
                   <Col md={4} xs={8} key={table.id}>
-                    <Table table={table} numMesa={table.numMesa} deleteTable={deleteTable}/>
+                    <Table
+                      table={table}
+                      numMesa={table.numMesa}
+                      deleteTable={deleteTable}
+                    />
                   </Col>
                 )
             )}
           {state.filter === 'ocupadas' &&
             tables.map(
-              (table) =>
+              table =>
                 table.estado === 'ocupada' && (
                   <Col md={4} xs={8} key={table.id}>
-                    <Table table={table} numMesa={table.numMesa} deleteTable={deleteTable}/>
+                    <Table
+                      table={table}
+                      numMesa={table.numMesa}
+                      deleteTable={deleteTable}
+                    />
                   </Col>
                 )
             )}
           {state.filter === 'reservadas' &&
             tables.map(
-              (table) =>
+              table =>
                 table.estado === 'reservada' && (
                   <Col md={4} xs={8} key={table.id}>
-                    <Table table={table} numMesa={table.numMesa} deleteTable={deleteTable}/>
+                    <Table
+                      table={table}
+                      numMesa={table.numMesa}
+                      deleteTable={deleteTable}
+                    />
                   </Col>
                 )
             )}
           <Col xs={8} md={4}>
-            <NewTable count={tables.length} addTable={addTable}/>
+            <NewTable count={tables.length} addTable={addTable} />
           </Col>
         </Row>
       </Col>
@@ -196,7 +211,7 @@ export default function Tables() {
   function onFilterChange(value) {
     if (value[0])
       setState({
-        filter: value[0],
+        filter: value[0]
       });
   }
 }
