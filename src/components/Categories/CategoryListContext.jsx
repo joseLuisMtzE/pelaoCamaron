f1e72b63d7e2b236618ad0e290076e4cb4540914
JonathanSC <jonathansolisc_22@hotmail.com>
2020-06-06 23:46:37 -0500

    Fixed issues

--- src/components/Categories/CategoryListContext.jsx
+++ src/components/Categories/CategoryListContext.jsx
@@ -11,7 +11,7 @@ const CategoryListContextProvider = props => {
 
   const retrieveCategories = async () => {
     try {
-      let response = await makeRequest('GET', 'categorias');
+      let response = await makeRequest('GET', 'categorias?isActive=true');
       let data = response.data.data;
       return data;
     } catch (err) {
@@ -95,10 +95,11 @@ const CategoryListContextProvider = props => {
   const addCategory = async name => {
     loader();
     let response = await addCategoryRequest(name);
-    setCategories([
-      ...categories,
-      { _id: response._id, nombre: response.nombre }
-    ]);
+    response &&
+      setCategories([
+        ...categories,
+        { _id: response._id, nombre: response.nombre }
+      ]);
   };
 
   const removeCategory = async id => {
